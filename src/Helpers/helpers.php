7061dd15a6ea1a6f92eeb353b75d17b93d10ac56
Lyos Anatoliy <les.anatoliy.92@gmail.com>
2019-10-01 23:59:58 +0600

    added helper

--- src/Helpers/helpers.php
+++ src/Helpers/helpers.php
@@ -1,15 +1,22 @@
 <?php
-use Avl\AdminManuals\Models\Manuals;
+use Avl\AdminManuals\Models\{ Manuals, ManualsData };
 
 if (!function_exists('getManualItems')) {
     function getManualItems($alias)
     {
-			$manuals = Manuals::whereAlias($alias)->first();
-			if (!is_null($manuals)) {
-				$manuals = $manuals->manual_data_childrens()->get();
-			}
-			return $manuals ?? null;
+		$manuals = Manuals::whereAlias($alias)->first();
+		if (!is_null($manuals)) {
+			$manuals = $manuals->manual_data_childrens()->get();
+		}
+		return $manuals ?? null;
     }
+}
+
+if (!function_exists('getManualItem')) {
+	function getManualItem ($id)
+	{
+		return ManualsData::find($id);
+	}
 }
 
  ?>
